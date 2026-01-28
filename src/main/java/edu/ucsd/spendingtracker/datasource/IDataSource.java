486e45db75d753cfefa63204e5987c386d91b6d8
dognutss <rickyzamfir@gmail.com>
2026-01-28 14:58:42 -0800

    Refactored ExpenseRepository to use IDataSource interface

--- src/main/java/edu/ucsd/spendingtracker/datasource/IDataSource.java
+++ src/main/java/edu/ucsd/spendingtracker/datasource/IDataSource.java
@@ -2,7 +2,12 @@ package edu.ucsd.spendingtracker.datasource;
 
 import java.util.List;
 
+import edu.ucsd.spendingtracker.model.Expense;
 
-public class IDataSource {
-    
+public interface IDataSource {
+    List<Expense> getExpenses();
+
+    void addExpense(Expense expense);
+
+    void deleteExpense(int id);
 }
