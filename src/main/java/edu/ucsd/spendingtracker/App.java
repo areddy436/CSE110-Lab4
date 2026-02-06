8fdb3437a8cf3ef0a0ae7ff7c960dbfd6272291d
dognutss <rickyzamfir@gmail.com>
2026-02-06 14:48:29 -0800

    Added spending charts in summary view

--- src/main/java/edu/ucsd/spendingtracker/App.java
+++ src/main/java/edu/ucsd/spendingtracker/App.java
@@ -1,7 +1,8 @@
 package edu.ucsd.spendingtracker;
 
-import edu.ucsd.spendingtracker.datasource.IDataSource;
-import edu.ucsd.spendingtracker.datasource.InMemoryDataSource;
+import java.util.ArrayList;
+import java.util.List;
+
 import edu.ucsd.spendingtracker.datasource.SqlDataSource;
 import edu.ucsd.spendingtracker.model.Model;
 import edu.ucsd.spendingtracker.presenter.PresenterManager;
@@ -10,15 +11,16 @@ import edu.ucsd.spendingtracker.presenter.SummaryPresenter;
 import edu.ucsd.spendingtracker.repository.ExpenseRepository;
 import edu.ucsd.spendingtracker.view.SpendingView;
 import edu.ucsd.spendingtracker.view.SummaryView;
+import edu.ucsd.spendingtracker.view.charts.BarChartProvider;
+import edu.ucsd.spendingtracker.view.charts.PieChartProvider;
+import edu.ucsd.spendingtracker.view.charts.IChartProvider;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class App extends Application {
     @Override
     public void start(Stage primaryStage) {
-        // InMemoryDataSource dataSource = InMemoryDataSource.getDefaultDataSource();
-        IDataSource dataSource = new SqlDataSource();
-
+        SqlDataSource dataSource = new SqlDataSource();
         ExpenseRepository repository = new ExpenseRepository(dataSource);
 
         Model sharedModel = new Model(repository);
@@ -26,8 +28,12 @@ public class App extends Application {
         SpendingView spendingView = new SpendingView();
         SummaryView summaryView = new SummaryView();
 
+        List<IChartProvider> chartProviders = new ArrayList<>();
+        chartProviders.add(new BarChartProvider());
+        chartProviders.add(new PieChartProvider());
+
         SpendingPresenter listPresenter = new SpendingPresenter(sharedModel, spendingView);
-        SummaryPresenter summaryPresenter = new SummaryPresenter(sharedModel, summaryView);
+        SummaryPresenter summaryPresenter = new SummaryPresenter(sharedModel, summaryView, chartProviders);
 
         PresenterManager manager = new PresenterManager();
         manager.defineInteractions(primaryStage, "Spending Tracker", listPresenter, summaryPresenter);
